71c305e7fc5044cbcbfb6f85e3834a6be7a09298
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-07-10 12:48:32 +0300

    Hide the blinking console tail on linux.

--- src/SnakeApp/GameController.cs
+++ src/SnakeApp/GameController.cs
@@ -8,6 +8,8 @@ namespace SnakeApp
 	{
 		public async Task StartNewGame()
 		{
+			PrepareConsole();
+
 			var game = new Game(80, 25, 5, 100);
 			game.Start();
 
@@ -17,6 +19,21 @@ namespace SnakeApp
 				userInput = Console.ReadKey(true);
 				game.ReceiveInput(userInput.Key);
 			} while (userInput.Key != ConsoleKey.Q);
+			
+			RestoreConsole();
+		}
+
+		private void PrepareConsole()
+		{
+			// getting the current cursor visibility is not supported on linux, so just hide then restore it
+			Console.Clear();
+			Console.CursorVisible = false;
+		}
+
+		private void RestoreConsole()
+		{
+			Console.Clear();
+			Console.CursorVisible = true;
 		}
 	}
 }
